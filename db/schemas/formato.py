bcd740021ce6fcaeba701b9206efe485cac6b35e
lazaro20 <salvirialazaro@gmail.com>
2025-06-25 17:36:07 -0300

    Se crea el schema en singular y plural para el formato (con base al modelo)

---
+++ db/schemas/formato.py
@@ -0,0 +1,9 @@
+def formato_schema (tipo)->dict:
+        return {
+            "id"                  : str(tipo["_id"]),
+            "nombre_formato"      : str(tipo["nombre_formato"]).lower().strip(),
+            "descripcion"         : str(tipo["descripcion"]).strip(),
+            }
+        
+def formatos_schemas(tipos)->list:
+    return [formato_schema(tipo) for tipo in tipos]
