6e2abbf5250509d83d579db59fe46d466d705b1b
lazaro20 <salvirialazaro@gmail.com>
2025-06-25 17:44:02 -0300

    se importan las librerias para el object_id, invalid_id

--- routers/formato.py
+++ routers/formato.py
@@ -1,2 +1,6 @@
 from fastapi import FastAPI
 from db.client import db_client
+from db.models import formato
+from db.schemas.formato import formato_schema , formatos_schemas
+from bson import ObjectId
+from bson.errors import InvalidId
