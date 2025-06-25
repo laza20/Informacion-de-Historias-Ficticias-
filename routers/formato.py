676afb0f2e54eda55a0a6d54a6f98d2f35f81c48
lazaro20 <salvirialazaro@gmail.com>
2025-06-25 17:47:45 -0300

    Se agrega el tag para diferenciar al formato en la documentacion y la url. se agrega la funcion para verificar si un id es valido (todo en formato)

--- routers/formato.py
+++ routers/formato.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, HTTPException, status
 from db.client import db_client
 from db.models import formato
 from db.schemas.formato import formato_schema , formatos_schemas
 from bson import ObjectId
 from bson.errors import InvalidId
+
+
+router = APIRouter( prefix="Formato",
+                   tags=["Formato"],
+                   responses={404:{ "message":"No encontrado"}})
+
+def validate_object_id(id: str):
+    try:
+        return ObjectId(id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="ID inválido")
