f63bff150eadb3ea0daa7dd63f0e1e44c3a308f3
lazaro20 <salvirialazaro@gmail.com>
2025-06-26 17:06:44 -0300

    Se crea el router para las historias (generales). se incluye el router en el main y se incluye el validate id en el router historias.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from routers import formato
+from routers import formato, historias
 
 
 app = FastAPI()
 
 app.include_router(formato.router)
+app.include_router(historias.router)
--- routers/historias.py
+++ routers/historias.py
@@ -1,2 +1,15 @@
 from fastapi import APIRouter, HTTPException, status
 from db.client import db_client
+from bson import ObjectId
+from bson.errors import InvalidId
+
+router = APIRouter( prefix="/Historias",
+                   tags=["Historias"],
+                   responses={404:{ "message":"No encontrado"}})
+
+
+def validate_object_id(id: str):
+    try:
+        return ObjectId(id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="ID inválido")
