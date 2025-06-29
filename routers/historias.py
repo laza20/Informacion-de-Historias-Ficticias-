8956de0533a9e0a0c84572f3be94c1655b891e4a
lazaro20 <salvirialazaro@gmail.com>
2025-06-29 16:19:45 -0300

    Se crea la posibilidad de cargar mas de una historia al mismo tiempo.

--- routers/historias.py
+++ routers/historias.py
@@ -37,3 +37,32 @@ async def create_one(historia:Historias):
     new_historia = historia_schema(db_client.Historias.find_one({"_id":id}))
     
     return new_historia
+
+@router.post("/Cargar/Muchos", response_model=list[Historias], status_code=status.HTTP_201_CREATED)
+async def create_many(historias:list[Historias]):
+
+    lista_historias = []
+    for historia in historias:
+        filtros = {
+            "nombre_de_la_historia":historia.nombre_de_la_historia,
+            "year": historia.year,
+            "formato":historia.formato
+            }
+        if db_client.Historias.find_one(filtros):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="La historia ingresada ya se encuentra en nuestra base de datos")
+    
+        if not db_client.Formatos.find_one({"nombre_formato":historia.formato}):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El formato ingresado es incorrecto o no se encuentra en la base de datos")
+        
+        dict_historia = dict(historia)
+        del dict_historia["id"]
+        dict_historia["tipo"] = "historia"
+        
+        lista_historias.append(dict_historia)
+        
+    resultado = db_client.Historias.insert_many(lista_historias)
+    ids = resultado.inserted_ids
+    documentos = db_client.Historias.find({"_id":{"$in":ids}})
+    
+    return historias_schema(documentos)
+        
