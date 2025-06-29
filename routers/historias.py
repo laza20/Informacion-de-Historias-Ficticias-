020d5b5faf96b6715061a3d6545885823eefaf49
lazaro20 <salvirialazaro@gmail.com>
2025-06-29 16:29:40 -0300

    Se reorganizaron las funciones para cargar una y muchas historias con una funcion aparte para poder optimizar codigo (evitar codigo duplicado y mejor disposicion del codigo)

--- routers/historias.py
+++ routers/historias.py
@@ -18,20 +18,7 @@ def validate_object_id(id: str):
     
 @router.post("/Cargar/Uno", response_model=Historias, status_code=status.HTTP_201_CREATED)
 async def create_one(historia:Historias):
-    filtros = {
-        "nombre_de_la_historia":historia.nombre_de_la_historia,
-        "year": historia.year,
-        "formato":historia.formato
-    }
-    if db_client.Historias.find_one(filtros):
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="La historia ingresada ya se encuentra en nuestra base de datos")
-    
-    if not db_client.Formatos.find_one({"nombre_formato":historia.formato}):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El formato ingresado es incorrecto o no se encuentra en la base de datos")
-    
-    dict_historia = dict(historia)
-    del dict_historia["id"]
-    dict_historia["tipo"] = "Historia"
+    dict_historia = cargar(historia)
     
     id = db_client.Historias.insert_one(dict_historia).inserted_id
     new_historia = historia_schema(db_client.Historias.find_one({"_id":id}))
@@ -43,6 +30,16 @@ async def create_many(historias:list[Historias]):
 
     lista_historias = []
     for historia in historias:
+        dict_historia = cargar(historia)
+        lista_historias.append(dict_historia)
+        
+    resultado = db_client.Historias.insert_many(lista_historias)
+    ids = resultado.inserted_ids
+    documentos = db_client.Historias.find({"_id":{"$in":ids}})
+    
+    return historias_schema(documentos)
+
+def cargar(historia):
         filtros = {
             "nombre_de_la_historia":historia.nombre_de_la_historia,
             "year": historia.year,
@@ -57,12 +54,4 @@ async def create_many(historias:list[Historias]):
         dict_historia = dict(historia)
         del dict_historia["id"]
         dict_historia["tipo"] = "historia"
-        
-        lista_historias.append(dict_historia)
-        
-    resultado = db_client.Historias.insert_many(lista_historias)
-    ids = resultado.inserted_ids
-    documentos = db_client.Historias.find({"_id":{"$in":ids}})
-    
-    return historias_schema(documentos)
-        
+        return dict_historia
