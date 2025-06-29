9c31fb3879d661e677c42a01b046d1e3840fccc6
lazaro20 <salvirialazaro@gmail.com>
2025-06-29 16:56:51 -0300

    Se modificaron los schemas, para que a la hora de retornar los datos sean o bien datos capitalizados o title (primera letra en mayuscula)

--- db/schemas/formato.py
+++ db/schemas/formato.py
@@ -1,9 +1,9 @@
 def formato_schema (tipo)->dict:
         return {
             "id"                  : str(tipo["_id"]),
-            "nombre_formato"      : str(tipo["nombre_formato"]).lower().strip(),
+            "nombre_formato"      : str(tipo["nombre_formato"]).title().strip(),
             "descripcion"         : str(tipo["descripcion"]).strip(),
-            "tipo"                : str(tipo["tipo"]).lower().strip()
+            "tipo"                : str(tipo["tipo"]).capitalize().strip()
             }
         
 def formatos_schemas(tipos)->list:
--- db/schemas/historias.py
+++ db/schemas/historias.py
@@ -1,11 +1,11 @@
 def historia_schema (tipo)->dict:
         return {
             "id"                         : str(tipo["_id"]),
-            "nombre_de_la_historia"      : str(tipo["nombre_de_la_historia"]).lower().strip(),
+            "nombre_de_la_historia"      : str(tipo["nombre_de_la_historia"]).title().strip(),
             "descripcion"                : str(tipo["descripcion"]).strip(),
             "year"                       : int(tipo["year"]),
-            "formato"                    : str(tipo["formato"]).lower().strip(),
-            "tipo"                       : str(tipo["tipo"]).lower().strip()
+            "formato"                    : str(tipo["formato"]).title().strip(),
+            "tipo"                       : str(tipo["tipo"]).capitalize().strip()
             }
         
 def historias_schema(tipos)->list:
