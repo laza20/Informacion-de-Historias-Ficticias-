3bd157b7f571d4384ff332cd4b671c4a1e806b79
lazaro20 <salvirialazaro@gmail.com>
2025-06-30 13:39:26 -0300

    Se crea una funcion para cargar muchos datos a la vez en la carpeta de funciones/funciones logicas. Disponible ya en routers/Formatos.py

--- funciones/funciones_logicas.py
+++ funciones/funciones_logicas.py
@@ -2,6 +2,7 @@ from bson import ObjectId
 from bson.errors import InvalidId
 from fastapi import HTTPException, status
 from db.client import db_client
+from funciones import validaciones
 
 def validate_object_id(id: str):
     try:
@@ -17,4 +18,17 @@ def cargar_uno(dato, base_de_datos, schema):
         new_formato = schema(coleccion.find_one({"_id":id}))
         return new_formato
     
+def cargar_muchos(datos, base_de_datos , schema, validacion):
+    coleccion = getattr(db_client, base_de_datos)
+    lista = []
+    for dato in datos:
+        validacion(dato, base_de_datos)
+        dict_dato = dict(dato)
+        del dict_dato["id"]
+        lista.append(dict_dato)
+        
+    resultado = coleccion.insert_many(lista)
+    ids = resultado.inserted_ids
+    documentos = coleccion.find({"_id":{"$in":ids}})
+    return schema(documentos)
     
