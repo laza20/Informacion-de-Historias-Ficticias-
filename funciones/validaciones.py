fdca88ca3b12de669a3d76c8944a37f46d80064d
lazaro20 <salvirialazaro@gmail.com>
2025-07-01 13:33:45 -0300

    Realizar modificaciones por bugs en los nombres de los atributos

--- funciones/validaciones.py
+++ funciones/validaciones.py
@@ -23,21 +23,21 @@ def validaciones_de_carga_historias(dato, base_de_datos):
 def validacion_de_carga_caracteristicas(dato, base_de_datos):
     coleccion = getattr(db_client, base_de_datos)
     filtro_historia = {        
-        "nombre_de_la_historia":{"$regex": f"^{dato.nombre_de_la_historia}$", "$options": "i"},
+        "nombre_de_la_historia":{"$regex": f"^{dato.nombre_historia}$", "$options": "i"},
         "formato":{"$regex": f"^{dato.formato}$", "$options": "i"}
     }
     
     filtro = {
         "caracteristica":{"$regex": f"^{dato.caracteristica}$", "$options": "i"},
-        "nombre_de_la_historia":{"$regex": f"^{dato.nombre_de_la_historia}$", "$options": "i"},
+        "nombre_historia":{"$regex": f"^{dato.nombre_historia}$", "$options": "i"},
         "formato":{"$regex": f"^{dato.formato}$", "$options": "i"},
         "identificacion":{"$regex": f"^{dato.identificacion}$", "$options": "i"}
     }
     if not db_client.Formatos.find_one({"nombre_formato":{"$regex": f"^{dato.formato}$", "$options": "i"}}):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"El formato {dato.formato} ingresado es incorrecto o no se encuentra en la base de datos")
     
-    if db_client.Historias.find_one(filtro_historia):
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="La historia ingresada ya se encuentra en nuestra base de datos")
+    if not db_client.Historias.find_one(filtro_historia):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="La historia ingresada no se encuentra en nuestra base de datos")
     
     if coleccion.find_one(filtro):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Caracteristica existente en la Base de datos")
