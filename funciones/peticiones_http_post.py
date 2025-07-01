ee8a5de756d41b020bf5f4ae3dde8ebaef39b139
lazaro20 <salvirialazaro@gmail.com>
2025-07-01 14:40:11 -0300

    Crear peticion http la cual se encarga de cargar muchos documentos a la vez por medio de parametros.

--- funciones/peticiones_http_post.py
+++ funciones/peticiones_http_post.py
@@ -2,7 +2,7 @@
 from funciones import funciones_logicas
 from fastapi import status, Body
 from pydantic import BaseModel
-from typing import Type, Any
+from typing import Type, Any, List
 
 
 
@@ -13,3 +13,9 @@ def cargar_uno(Clase: Type[BaseModel], router, base_de_datos, schema, validacion
         new_document = funciones_logicas.cargar_uno(clase, base_de_datos, schema)
     
         return new_document
+    
+def cargar_muchos(Clase: Type[BaseModel], router, base_de_datos, schema, validacion):
+    @router.post("/Cargar/Muchos", response_model=List[Clase], status_code=status.HTTP_201_CREATED)
+    async def create_many(clase: List[Clase] = Body(...)):
+        documentos = funciones_logicas.cargar_muchos(clase, base_de_datos, schema, validacion)
+        return documentos
