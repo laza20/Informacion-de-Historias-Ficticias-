9977671626b5e7d50cdcb57408b0018eb03b03bb
lazaro20 <salvirialazaro@gmail.com>
2025-07-01 12:44:28 -0300

    Solucion de un bug menor en el tag

--- routers/caracteristicas.py
+++ routers/caracteristicas.py
@@ -2,9 +2,17 @@ from fastapi import APIRouter, HTTPException, status
 from db.client import db_client
 from db.models.caracteristicas import Caracteristicas
 from db.schemas.caracteristicas import caracterica_schema,caractericas_schema
-from funciones import funciones_logicas
+from funciones import funciones_logicas, peticiones_http_post, validaciones
 
 router = APIRouter(prefix="/Caracteristicas",
-                   tags= Caracteristicas,
+                   tags= ["Caracteristicas"],
                    responses={404:{"message":"No encontrado"}}
+)
+
+peticiones_http_post.cargar_uno(
+    Caracteristicas,
+    router,
+    "Caracteristicas",
+    caracterica_schema,
+    validaciones.validaciones_de_carga_historias
 )
