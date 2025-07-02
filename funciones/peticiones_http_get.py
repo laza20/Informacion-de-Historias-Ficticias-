576433d5566f8c7f4f43d803f4859aba26b2c8e6
lazaro20 <salvirialazaro@gmail.com>
2025-07-02 10:34:14 -0300

    Feat: Crear una Nueva funcionalidad para poder visualizar todos los datos en una hoja externa la cual permite mejorar el mantenimiento del codigo y facilita la comprencion del mismo

--- funciones/peticiones_http_get.py
+++ funciones/peticiones_http_get.py
@@ -1,7 +1,15 @@
 # pyright: reportInvalidTypeForm=false
-from funciones import funciones_logicas
-from fastapi import status
 from pydantic import BaseModel
-from typing import Type, Any, List
+from fastapi import HTTPException, status
+from db.client import db_client
+from typing import Type, List
 
-
+def ver_todos(router, Clase: Type[BaseModel], schema, base_de_datos):
+    @router.get("/Todos", response_model=List[Clase])
+    async def view_olds():
+        coleccion = getattr(db_client, base_de_datos)
+        datos = schema(list(coleccion.find({"tipo": base_de_datos})))
+        if not datos:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No se encontraron o no hay datos")
+        
+        return datos
