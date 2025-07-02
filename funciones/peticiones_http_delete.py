e61c12d6b794cfb3d0451cacfb8a9e32b09eb60c
lazaro20 <salvirialazaro@gmail.com>
2025-07-02 15:06:38 -0300

    Feat(peticiones_http_delete.py): Crear funcion para eliminar datos por medio de una propiedad que no se puede repetir

--- funciones/peticiones_http_delete.py
+++ funciones/peticiones_http_delete.py
@@ -10,3 +10,15 @@ def borrar_todos(router, base_de_datos):
         borrados = coleccion.delete_many({"tipo":base_de_datos})
         if not borrados:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="En este momento no hay formatos guardados")
+        
+def borrar_por_data_string(router, base_de_datos, lista_propiedades):
+    @router.delete("/Eliminar/Por/{dato}",status_code=status.HTTP_202_ACCEPTED)
+    async def delete_by_data(dato:str):
+        coleccion = getattr(db_client, base_de_datos)
+        for propiedad in lista_propiedades:
+            borrado = coleccion.find_one_and_delete({propiedad:dato})
+            if borrado:
+                return {"mensaje": f"Documento con {propiedad} = '{dato}' eliminado correctamente"}
+        
+        if not borrado:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Dato {dato} incorrecto")
